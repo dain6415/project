7eb4bced4c6865612f2cd679088539229a4466e5
dain6415 <ekdlscjs4@naver.com>
2024-12-16 21:57:41 +0900

    1649846546546

--- 1010_chung/script/sect5.js
+++ 1010_chung/script/sect5.js
@@ -1,46 +1,60 @@
-const sect5Tabs = document.querySelectorAll('.contents-list li')
-sect5Tabs.forEach((tabMenu)=>{
-  tabMenu.addEventListener('click', function(e){
+const sect5Tabs = document.querySelectorAll(".contents-list li");
+sect5Tabs.forEach((tabMenu) => {
+  tabMenu.addEventListener("click", function (e) {
     e.preventDefault();
-  })
-})
+  });
+});
 
-
-// review
+// review, news
 const mySwiper2 = new Swiper(".mySwiper2", {
   // slidesPerView: 1,
   // spaceBetween: 10,
   loop: true,
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".next",
+    prevEl: ".prev",
   },
   breakpoints: {
     640: {
       slidesPerView: 1,
-      spaceBetween: 10,
+      spaceBetween: 30,
     },
     641: {
       slidesPerView: 2,
-      spaceBetween: 10,
+      spaceBetween: 30,
     },
     1000: {
       slidesPerView: 4,
-      spaceBetween: 60,
+      spaceBetween: 40,
     },
   },
 });
 
-//  photo
+//  photo, video
 const mySwiper3 = new Swiper(".mySwiper3", {
-  slidesPerView: 4,
-  grid: {
-    rows: 2,
+  loop: true,
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
   },
-  spaceBetween: 30,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+  breakpoints: {
+    568: {
+      slidesPerView: 1,
+      loop:false,
+      spaceBetween: 30,
+    },
+    641: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      grid:false,
+    },
+    1000: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 30,
+    },
   },
+  // autoHeight: true, // 자동 높이 계산
 });

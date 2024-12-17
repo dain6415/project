b7b52258fffb4925d269e1bc00d83d5300261292
dain6415 <ekdlscjs4@naver.com>
2024-12-17 12:22:15 +0900

    청

--- 1010_chung/script/header.js
+++ 1010_chung/script/header.js
@@ -1,3 +1,19 @@
+// 헤더 호버하면 이미지 변경
+const header = document.querySelector("header");
+const img = document.querySelector("header h1 img");
+const menu = document.querySelector('li.btn span')
+
+header.addEventListener("mouseover", function () {
+  img.src = "./img/logo_black.png";
+  // menu.style.background = "#000"
+});
+
+header.addEventListener("mouseout", function () {
+  img.src = "./img/logo.png";
+  // menu.style.background = "#fff"
+});
+
+
 // 반응형 피시버전 메뉴
 $("nav ul li.main-menu").mouseenter(function () {
   $(this).find("ul.sub-menu").stop().fadeIn(200);
@@ -6,32 +22,23 @@ $("nav ul li.main-menu").mouseleave(function () {
   $(this).find("ul.sub-menu").stop().fadeOut(200);
 });
 
-// 헤더 호버하면 이미지 변경
-const header = document.querySelector("header");
-const img = document.querySelector("header h1 img");
-
-header.addEventListener("mouseover", function () {
-  img.src = "./img/logo_black.png";
-});
-
-header.addEventListener("mouseout", function () {
-  img.src = "./img/logo.png";
-});
 
 // 반응형 폰 버전 메뉴
 $("li.main-menu").click(function () {
-  let $this = $(this);
-  let $smenu = $this.find(".s-menu");
-
-  if ($this.hasClass("on")) {
-    $this.removeClass("on").css("background", "");
-    $smenu.slideUp();
-  } else {
-    $("li.main-menu").removeClass("on").css("background", "");
-    $(".s-menu").slideUp();
-
-    $(this).addClass("on").css("background", "#e2475c");
-    $smenu.slideDown();
+  if (window.innerWidth <= 1000){
+    let $this = $(this);
+    let $smenu = $this.find(".s-menu");
+  
+    if ($this.hasClass("on")) {
+      $this.removeClass("on").css("background", "");
+      $smenu.slideUp();
+    } else {
+      $("li.main-menu").removeClass("on").css("background", "");
+      $(".s-menu").slideUp();
+  
+      $(this).addClass("on").css("background", "#e2475c");
+      $smenu.slideDown();
+    }
   }
 });
 

d84d8cba5840cc67cf178c7cb8d760184661d311
dain6415 <ekdlscjs4@naver.com>
2025-04-07 19:06:17 +0900

    new

--- p_NewJeans/script/index.js
+++ p_NewJeans/script/index.js
@@ -1,62 +1,58 @@
 // art1 --------------------------------------------------------------------------
 // item3 이미지 클릭시 이벤트 제어
-//꼭 아래처럼 이미지라고 써야..하는 거니? 이미지의 주소로 바뀌니까?
-  $('.art1 .item3 li a img').click(function(e){
-    e.preventDefault();
+$(".art1 .item3 li a img").click(function (e) {
+  e.preventDefault();
 
-    // 변수(근데 정확히 먼 차인지 모르겠음) 정의 = 현재 내가 누른 것의 src
-    var  imgSrc = $(this).attr('src');
+  var imgSrc = $(this).attr("src");
 
-    //img의 주소를 현재 내가 누른 이미지의 주소로 바꿔라
-    $('.popup-contents').attr('src',imgSrc);
-    $('.popup').fadeIn();
-  })
-  $('.close').click(function(){
-    $('.popup').fadeOut();
-  })
-
+  //img의 주소를 현재 내가 누른 이미지의 주소로 바꿔라
+  $(".popup-contents").attr("src", imgSrc);
+  $(".popup").fadeIn();
+});
+$(".close").click(function () {
+  $(".popup").fadeOut();
+});
 
 //item5 노래재생
 $(document).ready(function () {
-  const songs = [$("audio").get(0), $("audio").get(1)]; // 두 개의 곡 배열로 저장
-  let currentSongIndex = 0; // 현재 곡 인덱스
-  let isPlaying = false; // 재생 상태 체크
-
-  // 재생 토굴 버튼
-  $(".playbtn li:first-child").click(function (e) {
-    e.stopPropagation();
-    if (isPlaying) {
-      songs[currentSongIndex].pause();
-      $(this).find("img").attr("src", "./img/music-icon/art1-icon-play.png");
+  const songList = $("audio"); 
+  let nowPlaying = 0;
+  let playing = false;
+  
+  // 재생/멈춤 버튼
+  $(".playbtn li:first-child").click(function () {
+    const currentSong = songList.get(nowPlaying);
+    const playIcon = $(this).find("img");
+    
+    if (playing) {
+      currentSong.pause(); // 멈추기
+      playIcon.attr("src", "./img/music-icon/art1-icon-play.png");
     } else {
-      songs[currentSongIndex].play();
-      $(this).find("img").attr("src", "./img/music-icon/art1-icon-stop.png");
+      currentSong.play(); // 재생하기
+      playIcon.attr("src", "./img/music-icon/art1-icon-stop.png");
     }
-    isPlaying = !isPlaying;
+
+    playing = !playing; // 상태 바꾸기
   });
+  
+  // 다음 곡 버튼
+  $(".playbtn li:last-child").click(function () {
+    const currentSong = songList.get(nowPlaying);
+    currentSong.pause(); // 현재 곡 멈추고
+    currentSong.currentTime = 0; // 처음으로
+    
+    nowPlaying = (nowPlaying + 1) % songList.length; // 다음 곡으로!
+    const nextSong = songList.get(nowPlaying);
+    nextSong.play(); // 다음 곡 재생
 
-  $(".playbtn li:last-child").click(function (e) {
-    e.stopPropagation();
-    songs[currentSongIndex].pause(); // 현재 곡 멈춤
-    songs[currentSongIndex].currentTime = 0;
-    currentSongIndex = (currentSongIndex + 1) % songs.length; // 다음 곡 인덱스
-    songs[currentSongIndex].play(); // 다음 곡 재생
-    $(".playbtn li:first-child img").attr(
-      "src",
-      "./img/music-icon/art1-icon-stop.png"
-    );
-    // $('.play img').attr('src', './img/music-icon/art1-icon-stop.png'); // 아이콘 업데이트
-    $(".now .song").text(currentSongIndex === 0 ? "Supernatural" : "Right Now"); // 현재 곡 제목 업데이트
-    $(".now .singer").text("NewJeans"); // 가수 이름 업데이트
-    isPlaying = true; // 상태를 true로 설정
+    $(".playbtn li:first-child img").attr("src", "./img/music-icon/art1-icon-stop.png");
+    $(".now .song").text(nowPlaying === 0 ? "Supernatural" : "Right Now");
+
+    playing = true;
+    console.log("현재 재생할 곡:", currentSong);
   });
+  console.log("총 오디오 개수:", songList.length);
 });
-
-$(".playbtn li:first-child").click(function (event) {
-  event.stopPropagation(); // 이벤트 전파 방지
-  // 재생 또는 정지 로직
-});
-
 
 
 // art2 -----------------------------------------------------------------------------------
@@ -132,9 +128,6 @@ $(".choreo .next li").click(function () {
 
 // 현재 누른 li의 a를 찾아라. +
 
-
-
-
-$('.art3 a').click(function(e){
-  e.preventDefault()
-})
+$(".art3 a").click(function (e) {
+  e.preventDefault();
+});
